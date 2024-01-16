96a192aafe2f2cf8b3ed48a9663e035c4be7d6c0
snow <andy0509rey@gmail.com>
2024-01-16 22:18:25 +0100

    JavaBasics

---
+++ Tutorial/src/Entries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Entries {
+
+    public static void main(String[] args) {
+        // Vamos a leer entradas de usuario
+
+        //Definimos las variables
+        int a,b,c;
+        Scanner entrada = new Scanner(System.in);
+
+        //Código
+        System.out.println("Teclea un número :");
+        a = entrada.nextInt();
+
+        System.out.println("Teclear otro número :");
+        b = entrada.nextInt();
+
+        c = a * b;
+        System.out.println("a = " + a + " b = " + b + " a * b = " + c);
+    }
+}
+
